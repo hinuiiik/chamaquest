9e9928d7ddafb481925f198d93f8d89e8105154d
Anna Lynch <anna@fruitofthefirehose.org>
2025-02-08 16:18:49 -0500

    Defined types of database

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,9 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex-col gap-8 rounded-xl bg-blue-400 row-start-2 items-center sm:items-start">
-        <div className="flex flex-row justify-center">
+    <div
+      id="fullpage"
+      className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main
+        id="center"
+        className="flex-col gap-8 rounded-xl bg-blue-400 row-start-2 items-center sm:items-start p-3">
+        <div id="logoContainer" className="flex flex-row justify-center">
           <Image
             className="dark:invert m-4"
             src="/next.svg"
@@ -14,16 +18,18 @@ export default function Home() {
             priority
           />
         </div>
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
+        <div
+          id="list"
+          className="list-inside m-2 list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+          <p className="mb-2">
             Log in to the thingy!{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded-sm font-semibold">
               src/app/page.tsx
             </code>
             .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+          </p>
+          <p>Save and see your changes instantly.</p>
+        </div>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
@@ -49,50 +55,6 @@ export default function Home() {
           </a>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer">
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer">
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer">
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
