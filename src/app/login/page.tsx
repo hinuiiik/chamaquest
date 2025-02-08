46c6550c6bd40989eaae2252dedf26ded710cca1
Anna Lynch <anna@fruitofthefirehose.org>
2025-02-08 13:34:32 -0500

    Upgraded Tailwind to newest version (v4.0.0)

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Log in to the thingy!{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded-sm font-semibold">
               src/app/page.tsx
             </code>
             .
