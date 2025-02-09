ec047fa89cd9fad83d74af7a3ffdb63395f1f1d7
Vikram <github@vknc.mozmail.com>
2025-02-09 10:20:28 -0500

    fixed

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -14,19 +14,11 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-<<<<<<< HEAD
-        <body>
-        <div className={"overflow-hidden flex"}>
-        <div className={"bg-white h-screen w-64 text-black align p-3 border-r-4 border-pink-500"}>
-            <NavBar/>
-        </div>
-=======
         <body className={"fixed"}>
         <div className={"flex"}>
             <div className={"bg-gray-800 h-screen w-64 text-black align p-3 border-r-4 border-gray-700"}>
                 <NavBar/>
             </div>
->>>>>>> 7ca36e0d18f292019fe39abf8132dbd829e0a57e
             {children}
         </div>
         </body>
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,36 +1,6 @@
 export default function Home() {
     return (
         <>
-            {/*Webpage Container*/}
-
-                <div>
-                    <div className={"text-center h-10"}>
-                        <h1>Title</h1>
-                    </div>
-            {/*  Webpage Container  */}
-                    <div>
-                        <div className={"flex bg-yellow-50 w-[80vw] "}>
-                        {/*Info Container*/}
-                        <div className={"flex flex-col justify-center m-4"}>
-                            {/*Calendar component*/}
-                            <div className={"bg-red-500  w-80 h-64 mb-2"}>
-                                <p>calendar</p>
-                            </div>
-                            {/*Info Component*/}
-                            <div className={"bg-blue-400 w-80 h-48 mt-2"}>
-                                <p>info</p>
-                            </div>
-                        </div>
-                        {/*Chatbox Component*/}
-                        <div className={"bg-purple-800 flex w-[50vw] h-[86vh]  m-5"}>
-                            <p> chatbox</p>
-                        </div>
-                    </div>
-                    </div>
-
-                </div>
-
-
         </>
     );
 }
