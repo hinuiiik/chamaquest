afdb4d7683c08efff72dfa6a8e45b97fa8fded68
Vikram <github@vknc.mozmail.com>
2025-02-09 10:52:45 -0500

    Add sample money

--- src/app/ui/dashboard/info.tsx
+++ src/app/ui/dashboard/info.tsx
@@ -8,9 +8,11 @@ export default function InfoBox({ id, userId }: { id: number, userId: number}){
         <div className={"flex-col text-center self-center p-2"}>
             <div className={"bg-orange-400 pb-[9%] mb-2"}>
                 <h1>Total Pot</h1>
+                <h2>$100</h2>
             </div>
             <div className={"bg-green-400 pb-[9%] mb-2"}>
                 <h1>My Deposit</h1>
+                <h2>$5</h2>
             </div>
             <div className={"bg-orange-400 pb-[9%]"}>
                 <h1>Deposit Deadline: {endOfMonth}</h1>
